418d6aa5042cfff25600aa6f9d88ed8d0a96c927
diniashfiyadewi <diniashfiyadewi@gmail.com>
2023-09-30 13:20:37 +0700

    helloword

--- bin/array.dart
+++
@@ -1,8 +0,0 @@
-void main(){
-  var nama = ["Rafi", "Grace", "yapet", "Cinta"];
-  var umur = [30,25,20,15];
-
-var getName = nama[3];
-var usia = umur[3];
-  print("nama saya adalah $getName[3]");
-}
