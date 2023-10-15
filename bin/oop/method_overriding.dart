76008df3905ebb65b8f9d9f9294f5cb034c43e0a
diniashfiyadewi <diniashfiyadewi@gmail.com>
2023-10-15 17:20:37 +0700

    latihan2_OOP

---
+++ bin/oop/method_overriding.dart
@@ -0,0 +1,31 @@
+class Manager {
+  String? name;
+
+  void sayHello(String name){
+    print('Hello $name, my name is Manager ${this.name}');
+  }
+}
+
+class VicePresident extends Manager{
+
+  void sayHello(String name){
+    print('Hello $name, my name is VP ${this.name}');
+  }
+
+}
+
+class CLevel extends Manager {
+  void sayHello(String name){
+    print('hello $name, my name CLevel ${this.name}');
+  }
+}
+
+void main(){
+  var manager = Manager();
+  manager.name = 'Dini';
+  manager.sayHello('Deya');
+
+  var vp = VicePresident();
+  vp.name = 'Fitri';
+  vp.sayHello('Deya');
+}
